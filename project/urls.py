547e0e651007e2dd2a1b39ed2397b5f538fa9b0f
ainzzzer <ussin.batyr@gmail.com>
2025-03-13 19:57:35 +0500

    tokens app

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/", include("tokens.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        # Автогенерация документации
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"), # don't remove
+        path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",),
+        path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
